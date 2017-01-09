3b42eda274030f05154654f1e9ed20e4f408dd32
EddieCurtis <e.j.curtis1@gmail.com>
2017-01-08 21:18:24 -0500

    Committed template

---
+++ templates/go_template.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+const input string = "input_file.txt"
+
+func main() {
+	data, err := ioutil.ReadFile(input)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	} else {
+		lines := strings.Split(string(data), "\n")
+		// Remove first value as that just defines number of input lines
+		for i, next := range lines[1:] {
+			if next != "" {
+				result := processLine(next)
+				fmt.Printf("Case #%d: %s\n", i+1, result)
+			}
+		}
+	}
+}
+
+func processLine(line string) string {
+	// Implement solution here
+	return ""
+}
